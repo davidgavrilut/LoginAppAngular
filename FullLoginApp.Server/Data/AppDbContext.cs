65401d779f1599363ac025b2a655f8548c6b2760
davidgavrilut <davidgavrilut1@gmail.com>
2025-03-28 14:24:06 +0200

    Migrations, models and project setup

---
+++ FullLoginApp.Server/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using FullLoginApp.Server.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace FullLoginApp.Server.Data
+{
+    public class AppDbContext : IdentityDbContext<AppUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            
+        }
+    }
+}
